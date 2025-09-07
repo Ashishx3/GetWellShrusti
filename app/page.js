3b13628fdf3e9349bc805807cd672db71e6499cd
AshishX3 <ashishsinghyt111@gmail.com>
2025-09-07 20:00:40 +0530

    done

--- app/page.js
+++ app/page.js
@@ -1,17 +1,17 @@
 import Greeting from "@/components/Greeting";
 import Playlist from "@/components/Playlist";
 import Gallery from "@/components/Gallery";
-import CarePackage from "@/components/CarePackage";
 import BreathingExercise from "@/components/BreathingExercise";
+import CarePackage from "@/components/Carepackage";
 
 export default function Home() {
   return (
     <main className=" min-h-screen p-6 space-y-10 bg-gradient-to-br from-pink-100 via-purple-100 to-blue-100">
-      <Greeting />
-      <Playlist />
-      <Gallery />
-      <CarePackage />
-      <BreathingExercise />
+      <Greeting/>
+      <Playlist/>
+      <Gallery/>
+      <CarePackage/>
+      <BreathingExercise/>
     </main>
   );
 }
